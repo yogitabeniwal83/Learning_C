30e518a1fca902af52395203736d24c3633cd2a7
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-17 10:34:58 +0530

    Add for loop file

---
+++ for1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,n;
+    printf("enter number\n");
+    scanf("%d,&n");
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=1; j++)
+        {
+             printf("%d",j);
+        }
+        printf("\n");
+    }
+}
