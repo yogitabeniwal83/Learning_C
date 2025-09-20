0777b5ec24357baa102c9734daf5635790dbfbe4
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-20 20:34:03 +0530

    Add new while file

---
+++ while1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+    printf("enter number");
+    scanf("%d,&num");
+    while('0')
+    {
+        printf("hii");
+    }
+    return 0;
+}
