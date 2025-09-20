b3919bf8b2f418436e8df015ee1c730cedd0453b
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-20 20:23:21 +0530

    Add while loop file

---
+++ while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+    printf("enter number");
+    scanf("%d,&num");
+    while('A')
+    {
+        printf("hello");
+    }
+    return 0;
+}
