fa099349a21c2d4300ca37da59916964eb4c380e
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-20 20:35:19 +0530

    Add switch file

---
+++ switch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case1:
+        printf("A");
+        break;
+        case2:
+        printf("B");
+        break;
+        case3:
+        printf("C");
+        break;
+        default:
+        printf("D");
+    }
+    return 0;
+}
