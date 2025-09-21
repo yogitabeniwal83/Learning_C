3dbf0c950f06e88f0e4d0d00ed10527c44c081b5
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-21 23:29:49 +0530

    Add subtraction file

---
+++ subtraction.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter no");
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("subtraction of two number is %d",c);
+    return 0;
+}
