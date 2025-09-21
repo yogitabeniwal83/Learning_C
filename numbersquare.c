1671f091bf3d201cecfcaf17ee0da9c624bcee02
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-21 23:28:08 +0530

    Add new file

---
+++ numbersquare.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+         for(int i=1;i<=n;i++){
+            printf("1234");
+         }
+        printf("\n");
+    }
+        return 0;
+}
