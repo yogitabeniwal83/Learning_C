fa9f39a0822b1f46d6f557e6d631a08bfe1fd434
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-21 23:31:32 +0530

    Add Pattern File

---
+++ solidsquare.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter number of rows");
+    scanf("%d",&n);
+    int m;
+    printf("enter number of columns");
+    scanf("%d",&m);
+    for(int i=1;i<=4;i++){
+        for(int i=1;i<=4;i++){
+            printf("*");
+        }
+        printf("\n");
+    }
+ }
