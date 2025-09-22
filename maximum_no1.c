d3917fb5de763cfc17a149e305530be19c56e078
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-22 22:09:06 +0530

    Add new maximum number file

---
+++ maximum_no1.c
@@ -0,0 +1,12 @@
+// another way to find maximun number of interger 
+
+#include<stdio.h>
+#include<conio.h>
+#include<limits.h>
+int main() {
+    int x = INT_MAX;
+    printf("%d",x);
+    return 0;
+}
+
+
