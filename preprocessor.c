0c7b9feb9fe89fc5e16ffd782c7bab268d62d019
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2025-09-23 20:13:24 +0530

    Add maximum number of long data type file

---
+++ preprocessor.c
@@ -0,0 +1,14 @@
+
+// find maximun number of long data type
+
+#include<stdio.h>
+#include<math.h>
+#include<limits.h>
+
+int main() {
+
+    long x = LONG_MAX;
+    printf("%ld",x);
+
+    return 0;
+}
